7561fa00ce2f137ee19ae0cf0526fc735e01011d
Sven Svensson <ndrsofua@gmail.com>
2013-09-26 10:52:03 +0200

    Adding ability to listen to archived programs filtered by cateogry.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -12,12 +12,18 @@ def Start():
 def MainMenu():
 
     menu = ObjectContainer()
-
+    
+    #Add listen live menu.
     menu.add(
             DirectoryObject(
                 key = Callback(GetLiveChannels),
-                title='Lyssna live!')
-                )
+                title = 'Lyssna live!')
+            )
+    menu.add (
+            DirectoryObject(
+                key = Callback(GetAllCategories),
+                title = 'Program efter kategori')
+            )
     return menu
 
 
@@ -27,13 +33,73 @@ def GetLiveChannels():
 
     SRData = JSON.ObjectFromURL('http://api.sr.se/api/v2/channels?pagination=false&format=json')
     for channel in SRData['channels']:
-	ChannelMenu.add(
-		TrackObject(
-		    url = channel['liveaudio']['url'],
-		    title = channel['name'],
-		    summary = channel['tagline'],
-		    thumb = channel['image']
+        ChannelMenu.add(
+                TrackObject(
+                    url = channel['liveaudio']['url'],
+                    title = channel['name'],
+                    summary = channel['tagline'],
+                    thumb = channel['image']
+                    )
+                )
+
+    return ChannelMenu
+
+def GetAllCategories():
+
+    CategoryMenu = ObjectContainer(title1='Sveriges Radio', title2='Alla program')
+
+    SRData = JSON.ObjectFromURL('http://api.sr.se/api/v2/programcategories?format=json&pagination=false')
+
+    for Category in SRData['programcategories']:
+	CategoryMenu.add(
+		DirectoryObject(
+		    key = Callback(
+			ShowCategory, ChannelId = Category['id'], CategoryName = Category['name']),
+		    title = Category['name']
 		    )
 		)
 
-    return ChannelMenu
+
+    return CategoryMenu
+
+
+def ShowCategory(ChannelId, CategoryName):
+
+    ProgramMenu = ObjectContainer(title1='Sveriges Radio', title2=CategoryName)
+
+    SRData = JSON.ObjectFromURL('http://api.sr.se/api/v2/programs/index?pagination=false&format=json&programcategoryid='+str(ChannelId))
+
+    for Program in SRData['programs']:
+	ProgramMenu.add(
+		DirectoryObject(
+		    key= Callback(
+			ListEpisodes, ProgramId = Program['id'], ProgramName = Program['name']
+			),
+		    title = Program['name']
+		    )
+		)
+
+
+    return ProgramMenu
+
+def ListEpisodes(ProgramName, ProgramId):
+    Log("ProgramId: "+str(ProgramId))
+    EpisodeMenu = ObjectContainer(title1='Sveriges Radio', title2=ProgramName)
+    
+    SRData = JSON.ObjectFromURL('http://api.sr.se/api/v2/episodes/index?format=json&programid='+str(ProgramId))
+
+    for Episode in SRData['episodes']:
+	if 'broadcast' in Episode:
+	    Log("Found broadcast")
+	    EpisodeMenu.add(
+		    TrackObject(
+			url = Episode['broadcast']['broadcastfiles'][0]['url'],
+			title = Episode['title'],
+			summary = Episode['description'],
+			thumb = Episode['imageurl']
+			)
+		)
+	else:
+	    Log("There was no broadcast")
+
+    return EpisodeMenu
