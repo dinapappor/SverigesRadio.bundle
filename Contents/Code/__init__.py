fa85a1d35ff77099d6ad01eb2716a9278c0ddd9a
Sven Svensson <ndrsofua@gmail.com>
2013-09-26 11:53:56 +0200

    Added pagination for podcasts.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -82,24 +82,45 @@ def ShowCategory(ChannelId, CategoryName):
 
     return ProgramMenu
 
-def ListEpisodes(ProgramName, ProgramId):
+def ListEpisodes(ProgramName, ProgramId, Page = 1, TotalPages = 1):
     Log("ProgramId: "+str(ProgramId))
     EpisodeMenu = ObjectContainer(title1='Sveriges Radio', title2=ProgramName)
-    
-    SRData = JSON.ObjectFromURL('http://api.sr.se/api/v2/episodes/index?format=json&programid='+str(ProgramId))
+    if Page < TotalPages:
+	SRData = JSON.ObjectFromURL('http://api.sr.se/api/v2/episodes/index?format=json&programid='+str(ProgramId)+'&page='+str(Page))
+    else:
+	SRData = JSON.ObjectFromURL('http://api.sr.se/api/v2/episodes/index?format=json&programid='+str(ProgramId))
 
     for Episode in SRData['episodes']:
 	if 'broadcast' in Episode:
-	    Log("Found broadcast")
+	    Log(Episode['broadcast']['broadcastfiles'][0]['duration'])
 	    EpisodeMenu.add(
 		    TrackObject(
 			url = Episode['broadcast']['broadcastfiles'][0]['url'],
 			title = Episode['title'],
 			summary = Episode['description'],
-			thumb = Episode['imageurl']
+			thumb = Episode['imageurl'],
+			duration = Episode['broadcast']['broadcastfiles'][0]['duration'] * 1000
 			)
 		)
+	elif 'listenpodfile':
+	    Log(Episode['listenpodfile']['duration'])
+	    EpisodeMenu.add(
+		    TrackObject(
+			url = Episode['listenpodfile']['url'],
+			title = Episode['title'],
+			summary = Episode['description'],
+			duration = Episode['listenpodfile']['duration'] * 1000
+			)
+		    )
 	else:
-	    Log("There was no broadcast")
+	    pass
+
+    if SRData['pagination']['page'] < SRData['pagination']['totalpages']:
+	EpisodeMenu.add(
+		NextPageObject(
+		    key = Callback(ListEpisodes, ProgramName = ProgramName, ProgramId=ProgramId, Page = Page + 1, TotalPages = SRData['pagination']['totalpages'] ),
+		    title = u'Nästa sida'
+		    )
+		)
 
     return EpisodeMenu
